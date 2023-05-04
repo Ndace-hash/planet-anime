fba4e30e539f729a8573a81d50b1ddafdf83997c
Zaccheaus <zaccheausyisa00@gmail.com>
2023-05-04 14:25:57 +0100

    feat: created skeleton loaders form home screen

---
+++ src/components/loader/AnimeCardSkeleton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="w-[200px] h-[300px] shrink-0 object-fill overflow-hidden flex items-center justify-center cursor-pointer border-2 border-transparent rounded-lg"
+  ></div>
+</template>
+
+<style scoped>
+div {
+  animation: pulsing 3s linear infinite;
+}
+@keyframes pulsing {
+  0% {
+    background-color: #a3a3a3;
+  }
+  50% {
+    background-color: #514e4e;
+  }
+  100% {
+    background-color: #a3a3a3;
+  }
+}
+</style>
---
+++ src/components/loader/GenreDisplaySkeleton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="w-full h-[350px]">
+    <h2 class="w-24 h-4 text-xl capitalize my-2 rounded-xl"></h2>
+    <div class="w-full gap-2 flex overflow-x-scroll">
+      <AnimeCardSkeleton />
+      <AnimeCardSkeleton />
+      <AnimeCardSkeleton />
+      <AnimeCardSkeleton />
+      <AnimeCardSkeleton />
+      <AnimeCardSkeleton />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AnimeCardSkeleton from "./AnimeCardSkeleton.vue";
+</script>
+<style scoped>
+::-webkit-scrollbar {
+  display: none;
+}
+h2 {
+  animation: pulsing 2s linear infinite;
+}
+@keyframes pulsing {
+  0% {
+    background-color: #a3a3a3;
+  }
+  50% {
+    background-color: #514e4e;
+  }
+  100% {
+    background-color: #a3a3a3;
+  }
+}
+</style>
