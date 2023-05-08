54c5db80eb25746671eac97c8a5d2171f0d11001
Zaccheaus <zaccheausyisa00@gmail.com>
2023-05-08 11:16:30 +0100

    bug: Loader on home page was no showing so i change suspense to a normal reactive loading variable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,40 @@
 <template>
-  <Suspense>
-    <div>
-      <GenreDisplay
-        v-for="genre in genres"
-        :genre="genre.genre"
-        :link="genre.link"
-        :key="genre.link"
-      />
-    </div>
-    <template #fallback>
-      <div>
-        <GenreDisplaySkeleton />
-        <GenreDisplaySkeleton />
-        <GenreDisplaySkeleton />
-        <GenreDisplaySkeleton />
-        <GenreDisplaySkeleton />
-      </div>
-    </template>
-  </Suspense>
+  <h1 v-if="error !== null" class="text-xl text-red-400 font-bold">
+    Something went wrong! Check your internet connection and refresh page.
+  </h1>
+  <GenreDisplay
+    v-for="genre in genres"
+    :genre="genre.genre"
+    :link="genre.link"
+    :key="genre.link"
+    v-else
+  />
 </template>
 
 <script>
 import GenreDisplay from "../components/GenreDisplay.vue";
-import GenreDisplaySkeleton from "../components/loader/GenreDisplaySkeleton.vue";
-import { ref, onMounted, Suspense } from "vue";
+import { ref, onMounted } from "vue";
 import { axiosInstance } from "../composables/utils/Axios";
 export default {
   setup() {
     const genres = ref([]);
+    const error = ref(null);
     onMounted(async () => {
-      const { data } = await axiosInstance.get("/genres");
+      try {
+        const { data } = await axiosInstance.get("/genres");
 
-      for (let index = 0; index < 15; index++) {
-        genres.value.push(data[index]);
+        for (let index = 0; index < 15; index++) {
+          genres.value.push(data[index]);
+        }
+      } catch (e) {
+        error.value = { code: e.code, message: e.message };
       }
     });
 
-    return { genres };
+    return { genres, error };
   },
   components: {
     GenreDisplay,
-    GenreDisplaySkeleton,
-    Suspense,
   },
 };
 </script>
